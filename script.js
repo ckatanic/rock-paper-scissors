b3953669142512047bf55646095bda6e09e17b26
Chris <ckatanic@gmail.com>
2023-05-09 20:31:49 -0700

    work on getComputerChoice and playRound functions

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("This is a test");
+function getComputerChoice() {
+    let getComputerChoice = "";
+    let choiceOptions = ['Rock', 'Paper', 'Scissors'];
+    getComputerChoice = choiceOptions[Math.floor(Math.random() * 3)];
+    return getComputerChoice;
+}
+
+function playRound() {
+    let computerChoice = getComputerChoice();
+    let playerInput = prompt("Pick Rock, Paper or Scissors").toLowerCase().split("");
+    playerInput[0] = playerInput[0].toUpperCase();
+    let playerChoice = playerInput.join("");
+    // console.log("Computer's Choice: " + computerChoice);
+    // console.log("Player's Choice: " + playerChoice);
+    if (playerChoice === computerChoice) {
+        return "Draw!"
+    }
+    else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+        return "You win! Rock beats Scissors!";
+    }
+    else if (playerChoice === "Rock" && computerChoice === "Paper") {
+        return "You lose! Paper beats Rock!";
+    }
+    else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+        return "You lose! Scissors beats Rock!";
+    }
+    else if (playerChoice === "Paper" && computerChoice === "Rock") {
+        return "You win! Paper beats Rock!";
+    }
+    else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+        return "You lose! Scissors beats Paper!";
+    }
+    else if (playerChoice === "Paper" && computerChoice === "Rock") {
+        return "You win! Paper beats Rock!";
+    }
+}
