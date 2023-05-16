9d483238828521a90f59479571b510115602383e
Chris <ckatanic@gmail.com>
2023-05-15 21:10:56 -0700

    work on playGame function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let score = {
+    player: 0,
+    computer: 0
+}
+
 function getComputerChoice() {
     let getComputerChoice = "";
     let choiceOptions = ['Rock', 'Paper', 'Scissors'];
@@ -10,6 +15,10 @@ function playRound() {
     let playerInput = prompt("Pick Rock, Paper or Scissors").toLowerCase().split("");
     playerInput[0] = playerInput[0].toUpperCase();
     let playerChoice = playerInput.join("");
+    if (playerChoice != 'Rock' && playerChoice != 'Paper' && playerChoice != 'Scissors') {
+        alert('You did not enter a valid choice. Please Try again');
+        return playRound();
+    }
     // console.log("Computer's Choice: " + computerChoice);
     // console.log("Player's Choice: " + playerChoice);
     if (playerChoice === computerChoice) {
@@ -17,21 +26,43 @@ function playRound() {
         return playRound();
     }
     else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+        score.player += 1;
         return "You win! Rock beats Scissors!";
     }
     else if (playerChoice === "Rock" && computerChoice === "Paper") {
+        score.computer += 1;
         return "You lose! Paper beats Rock!";
     }
     else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+        score.computer += 1;
         return "You lose! Scissors beats Rock!";
     }
     else if (playerChoice === "Paper" && computerChoice === "Rock") {
+        score.player += 1;
         return "You win! Paper beats Rock!";
     }
-    else if (playerChoice === "Paper" && computerChoice === "Scissors") {
-        return "You lose! Scissors beats Paper!";
+    else if (playerChoice === "Scissors" && computerChoice === "Rock") {
+        score.computer += 1;
+        return "You lose! Rock beats Scissors!";
     }
-    else if (playerChoice === "Paper" && computerChoice === "Rock") {
-        return "You win! Paper beats Rock!";
+    else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+        score.player += 1;
+        return "You win! Scissors beats Paper!";
     }
+}
+
+function playGame() {
+
+
+    while (score.player <= 2 && score.computer <= 2 ) {
+        console.log(playRound());
+        console.log(score);
+    }
+
+    if (score.player === 3) {
+        console.log('Game Over: You Won!');
+    } else {
+        console.log("Game Over: You lost!")
+    }
+
 }
